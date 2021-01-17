c07fcacada9bf7b20020e0482e907dc0ee540259
Mahmoud Wardeh <>
2021-01-17 01:35:22 +0000

    Update home page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="max-w-7xl mx-auto px-5 py-7">
-    <div class="font-bold">
-      <div class="text-6xl mb-4 mt-5 text-gray-600">
+  <div class="max-w-7xl mx-auto px-5 py-7 w-full">
+    <div class="font-bold max-w-3xl text-left">
+      <div class="text-5xl sm:text-6xl mb-4 mt-5 text-gray-600 leading-tight">
         Switching off your camera
       </div>
-      <div class="text-5xl mb-4 text-blue-800">During a conference call</div>
-      <div class="text-5xl text-blue-800">Can reduce your</div>
-      <div class="text-5xl mb-4 -mt-6 mb-24 text-blue-800">
-        Carbon footprint by
-        <span class="text-8xl text-green-600">96%!</span>
+      <div class="text-5xl mb-4 text-blue-800 leading-tight max-w-2xl">
+        During a conference call can reduce your carbon footprint by
+        <span class="text-6xl text-green-600">96%!</span>
       </div>
     </div>
   </div>
