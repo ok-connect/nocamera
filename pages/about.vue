a888973938d4c5528cb2258f526309c76bb3bccf
Max Mahmoud Wardeh <m@lurnid.com>
2021-01-17 13:43:24 +0000

    Add about page

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-w-5xl mx-auto px-5 py-10 prose">
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('about').fetch()
+
+    return {
+      page,
+    }
+  },
+}
+</script>
